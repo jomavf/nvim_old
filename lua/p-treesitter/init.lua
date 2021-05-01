e5ee9062cfa5decf6034e7256b6403421aa7c9ed
jmvelizfr <jose.veliz.francia@gmail.com>
2021-05-01 09:45:18 -0500

    treesiter setup

---
+++ lua/p-treesitter/init.lua
@@ -0,0 +1,11 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = "all" -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = {},
+    highlight = {
+        enable = true
+    },
+    -- indent = {enable = true, disable = {"python", "html", "javascript"}},
+    -- TODO seems to be broken
+    indent = {enable = {"javascriptreact"}},
+    autotag = {enable = true},
+}
