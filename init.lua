fe1967ecbf2ce1ddeff49d6bfd05c0c685989ba3
Jose Veliz <jose.veliz@globant.com>
2021-05-02 14:03:37 -0500

    I'm trying to push just Init.LUA lets see if fugitive works as I want

--- init.lua
+++ init.lua
@@ -10,8 +10,10 @@ require('p-lspinstall')
 require('p-lspcompe')
 require('p-telescope')
 
+-- Git
+require('p-fugitive')
+
 -- LSP
 require('lsp')
 require('lsp.lua-ls')
-
 -- require('lsp.js-ts-ls')
