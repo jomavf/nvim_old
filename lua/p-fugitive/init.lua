c44dfe8056ec4b9835db7f31a510e5f1b0cfd274
Jose Veliz <jose.veliz.francia@gmail.com>
2021-05-02 17:29:45 -0500

    update fugitive galaxyline

---
+++ lua/p-fugitive/init.lua
@@ -0,0 +1,6 @@
+local vimGlobal = vim
+
+vimGlobal.cmd('nmap <leader>gh :diffget //3<CR>')
+vimGlobal.cmd('nmap <leader>gh :diffget //2<CR>')
+vimGlobal.cmd('nmap <leader>gs :G <CR>')
+vimGlobal.cmd('nmap <leader>gp :Git push <CR>')
