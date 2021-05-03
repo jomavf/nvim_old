9f5bf56ff6d571f28d50c7f19b1c983f21bb107a
Jose Veliz <jose.veliz.francia@gmail.com>
2021-05-02 21:00:11 -0500

    ignore haskell - bug in macos

--- lua/p-treesitter/init.lua
+++ lua/p-treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = {},
+    ensure_installed = "all",
+    ignore_install = {"haskell"},
     highlight = {
         enable = true
     },
